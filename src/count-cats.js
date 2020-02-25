e0f88557ba089dbd030aac2f42081d17c73541b7
LonelyDragoness <Starfade@yandex.by>
2020-02-25 17:41:15 +0300

    feat: Count cats!

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let number = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let item = 0; item < matrix[i].length; item++) {
+      if (matrix[i][item] === "^^") {
+        number += 1;
+      }
+    }
+  }
+  return number;
 };
