18099f1e2ad038dce81e5edf3bfbd8fa40f09578
LonelyDragoness <Starfade@yandex.by>
2020-02-25 21:06:22 +0300

    feat: add Dream team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let secretName = [];
+  if (!members) {return false}
+  else if (members.isArray ||  members !== []) {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof (members[i]) == 'string') {
+        secretName.push(members[i].trim()[0].toUpperCase());
+      }
+    }
+  } else return false;
+  return secretName.sort().join('');
 };
