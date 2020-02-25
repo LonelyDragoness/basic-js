aa1dc9790f8235d6d6a19c1a8e586d473ba142ec
LonelyDragoness <Starfade@yandex.by>
2020-02-25 19:25:23 +0300

    feat: add Carbon dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,11 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let activity = parseFloat(sampleActivity);
+  if (typeof(sampleActivity) !== 'string' || Number.isNaN(activity) || activity > 15 || activity <= 0) {
+    return false;
+  } else {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / activity) / (0.693 / HALF_LIFE_PERIOD));
+  }
 };
