ae4c4a34fc3d1eef5cb4ccfe361ab07aedf5575b
LonelyDragoness <Starfade@yandex.by>
2020-03-20 19:28:00 +0300

    feat: implement depth calculator function

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,10 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        let nestsCount = 0;
+        arr = arr.filter(func => Array.isArray(func));
+        arr.forEach(element => {
+            nestsCount = Math.max(nestsCount, this.calculateDepth(element))
+        });
+        return 1 + nestsCount
     }
 };
