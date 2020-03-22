ff1cedaeab1ed6fe2cde4811424c4a0452951b22
LonelyDragoness <Starfade@yandex.by>
2020-03-22 18:32:52 +0300

    feat: implement extended repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,23 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+    if (!options['repeatTimes']) { options['repeatTimes'] = 1; }
+    if (!options['separator']) { options['separator'] = '+'; }
+    if (!options['additionRepeatTimes']) { options['additionRepeatTimes'] = 1; }
+    if (!options['additionSeparator']) { options['additionSeparator'] = ''; }
+    if (!options['addition']) {
+        if(`${options['addition']}` !== 'false' && `${options['addition']}` !== 'null') {
+            options['addition'] = '';
+        } else {
+            options['addition'] = `${options['addition']}`;
+        }
+    }
+
+    let additionalPart = `${options['addition']}`;
+    for (let j = 0; j < options['additionRepeatTimes'] - 1; j++) {
+        additionalPart += `${options['additionSeparator']}${options['addition']}`
+    }
+    let result = `${str}${additionalPart}`;
+    for (let k = 0; k < options['repeatTimes'] - 1; k++) {
+        result += `${options['separator']}${str}${additionalPart}`
+    }
+    return result
+}
